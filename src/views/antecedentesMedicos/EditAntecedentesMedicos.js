4f4d44850244aa2aa1290c43204e81af95527275
PRACTICANTE 2 SOPORTE <practicantesoporte@ESUMER.LOCAL>
2024-12-16 08:51:12 -0500

    style edit show

--- src/views/antecedentesMedicos/EditAntecedentesMedicos.js
+++ src/views/antecedentesMedicos/EditAntecedentesMedicos.js
@@ -1,45 +1,150 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import {
+  Card,
+  CardContent,
+  Divider,
+  Box,
+  Typography,
+  TextField,
+  Button,
+} from "@mui/material";
 
-const URI_ANTECEDENTE_MEDICO = 'http://localhost:3001/antecedentesMedicos/'
+const URI_ANTECEDENTE_MEDICO = "http://localhost:3001/antecedentesMedicos/";
 
 const CompEditarAntecedentesMedicos = () => {
-    const [var_nombreAntecedenteMedico, setVar_nombreAntecedenteMedico] = useState('')
-    const navigate = useNavigate()
-    const {id_antecedenteMedicoPK} = useParams()
+  const [var_nombreAntecedenteMedico, setVar_nombreAntecedenteMedico] =
+    useState("");
+  const navigate = useNavigate();
+  const { id_antecedenteMedicoPK } = useParams();
 
-    //procedimiento para actualizar
-    const actualizar = async (e) => {
-        e.preventDefault()
-        await axios.put(URI_ANTECEDENTE_MEDICO+id_antecedenteMedicoPK, {
-            var_nombreAntecedenteMedico: var_nombreAntecedenteMedico
-        })
-        navigate('/app/antecedentesMedicos')
-    }
+  //procedimiento para actualizar
+  const actualizar = async (e) => {
+    e.preventDefault();
+    await axios.put(URI_ANTECEDENTE_MEDICO + id_antecedenteMedicoPK, {
+      var_nombreAntecedenteMedico: var_nombreAntecedenteMedico,
+    });
+    navigate("/app/antecedentesMedicos");
+  };
 
-    useEffect( ()=>{
-        getAntecedenteMedicoPorId()
-    }, [])
+  const handleGoBack = () => {
+    navigate("/app/antecedentesMedicos");
+  };
 
-    const getAntecedenteMedicoPorId = async ()=> {
-        const res = await axios.get(URI_ANTECEDENTE_MEDICO+id_antecedenteMedicoPK)
-        setVar_nombreAntecedenteMedico(res.data.var_nombreAntecedenteMedico)
-    }
-    return (
-        <div>
-            <h3>editar antecedente medico</h3>
-            <form onSubmit={actualizar}>
-                <div className="mb-3">
-                    <laber className="form-label" >nombre del antecedente medico</laber>
-                    <input value={var_nombreAntecedenteMedico} onChange={ (e)=> setVar_nombreAntecedenteMedico(e.target.value)} className="form-control" />
+  useEffect(() => {
+    getAntecedenteMedicoPorId();
+  }, []);
 
-                </div>
-                <button type="submit" className="btn btn-primary" >guardar</button>
-            </form>
-        </div>
-    )
+  const getAntecedenteMedicoPorId = async () => {
+    const res = await axios.get(
+      URI_ANTECEDENTE_MEDICO + id_antecedenteMedicoPK
+    );
+    setVar_nombreAntecedenteMedico(res.data.var_nombreAntecedenteMedico);
+  };
+  return (
+    <div
+      style={{
+        backgroundColor: "#F2F2F2",
+        paddingTop: "3%",
+        paddingBottom: "3%",
+      }}
+    >
+      <div
+        style={{ textAlign: "center", marginBottom: "1%", marginTop: "-1%" }}
+      >
+        <img
+          src="public/fondo_form.png"
+          alt="Descripción de la imagen"
+          style={{ width: "20%", height: "auto" }}
+        />
+      </div>
+      <Card
+        variant="outlined"
+        sx={{
+          p: 0,
+          width: "100%",
+          maxWidth: 800,
+          margin: "auto",
+          backgroundColor: "#F2F2F2",
+          borderColor: "#202B52",
+        }}
+      >
+        <Box sx={{ padding: "15px 30px" }} display="flex" alignItems="center">
+          <Box flexGrow={1}>
+            <Typography
+              sx={{
+                fontSize: "30px",
+                fontWeight: "500",
+                textAlign: "center",
+                color: "#202B52",
+                fontFamily: "Roboto Condensed",
+              }}
+            >
+              <strong>Editar antecedente medico</strong>
+            </Typography>
+          </Box>
+        </Box>
+        <Divider
+          style={{
+            marginLeft: "5%",
+            marginRight: "5%",
+            borderColor: "#202B52",
+          }}
+        />
+        <CardContent sx={{ padding: "30px" }}>
+          <form onSubmit={actualizar}>
+            <Typography
+              variant="h6"
+              sx={{
+                fontFamily: "Roboto Condensed",
+                color: "#202B52",
+                fontSize: "16px",
+              }}
+            >
+              Nombre antecedente medico:
+            </Typography>
+            <TextField
+              value={var_nombreAntecedenteMedico}
+              onChange={(e) => setVar_nombreAntecedenteMedico(e.target.value)}
+              fullWidth
+              sx={{ mb: 2 }}
+              InputProps={{
+                sx: {
+                  height: "40px",
+                  fontFamily: "Roboto Condensed",
+                  fontSize: "16px",
+                },
+              }}
+            />
 
-}
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                gap: "10px",
+              }}
+            >
+              <Button
+                sx={{ backgroundColor: "#202B52", fontFamily: "poppins" }}
+                variant="contained"
+                type="submit"
+              >
+                Guardar
+              </Button>
+              <Button
+                sx={{ backgroundColor: "#ff0000", fontFamily: "poppins" }}
+                variant="contained"
+                onClick={handleGoBack}
+              >
+                Volver
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
 
-export default CompEditarAntecedentesMedicos
+export default CompEditarAntecedentesMedicos;
--- src/views/estructuraOrganizacional/EditEstructuraOrganizacional.js
+++ src/views/estructuraOrganizacional/EditEstructuraOrganizacional.js
@@ -1,45 +1,148 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import {
+  Card,
+  CardContent,
+  Divider,
+  Box,
+  Typography,
+  TextField,
+  Button,
+} from "@mui/material";
 
-const URI_ESTRUCTURA_ORGANIZACIONAL = 'http://localhost:3001/estructuraOrganizacional/'
+const URI_ESTRUCTURA_ORGANIZACIONAL =
+  "http://localhost:3001/estructuraOrganizacional/";
 
 const CompEditarEstructuraOrganizacional = () => {
-    const [var_nombreArea, setVar_nombreArea] = useState('')
-    const navigate = useNavigate()
-    const {id_areaPk} = useParams()
+  const [var_nombreArea, setVar_nombreArea] = useState("");
+  const navigate = useNavigate();
+  const { id_areaPk } = useParams();
 
-    //procedimiento para actualizar
-    const actualizar = async (e) => {
-        e.preventDefault()
-        await axios.put(URI_ESTRUCTURA_ORGANIZACIONAL+id_areaPk, {
-            var_nombreArea: var_nombreArea
-        })
-        navigate('/app/estructuraOrganizacional')
-    }
+  //procedimiento para actualizar
+  const actualizar = async (e) => {
+    e.preventDefault();
+    await axios.put(URI_ESTRUCTURA_ORGANIZACIONAL + id_areaPk, {
+      var_nombreArea: var_nombreArea,
+    });
+    navigate("/app/estructuraOrganizacional");
+  };
 
-    useEffect( ()=>{
-        getEpsPorId()
-    }, [])
+  useEffect(() => {
+    getEpsPorId();
+  }, []);
 
-    const getEpsPorId = async ()=> {
-        const res = await axios.get(URI_ESTRUCTURA_ORGANIZACIONAL+id_areaPk)
-        setVar_nombreArea(res.data.var_nombreArea)
-    }
-    return (
-        <div>
-            <h3>editar estructura organizacional</h3>
-            <form onSubmit={actualizar}>
-                <div className="mb-3">
-                    <laber className="form-label" >nombre estructura organizacional</laber>
-                    <input value={var_nombreArea} onChange={ (e)=> setVar_nombreArea(e.target.value)} className="form-control" />
+  const getEpsPorId = async () => {
+    const res = await axios.get(URI_ESTRUCTURA_ORGANIZACIONAL + id_areaPk);
+    setVar_nombreArea(res.data.var_nombreArea);
+  };
 
-                </div>
-                <button type="submit" className="btn btn-primary" >guardar</button>
-            </form>
-        </div>
-    )
+  const handleGoBack = () => {
+    navigate("/app/estructuraOrganizacional");
+  };
+  return (
+    <div
+      style={{
+        backgroundColor: "#F2F2F2",
+        paddingTop: "3%",
+        paddingBottom: "3%",
+      }}
+    >
+      <div
+        style={{ textAlign: "center", marginBottom: "1%", marginTop: "-1%" }}
+      >
+        <img
+          src="public/fondo_form.png"
+          alt="Descripción de la imagen"
+          style={{ width: "20%", height: "auto" }}
+        />
+      </div>
+      <Card
+        variant="outlined"
+        sx={{
+          p: 0,
+          width: "100%",
+          maxWidth: 800,
+          margin: "auto",
+          backgroundColor: "#F2F2F2",
+          borderColor: "#202B52",
+        }}
+      >
+        <Box sx={{ padding: "15px 30px" }} display="flex" alignItems="center">
+          <Box flexGrow={1}>
+            <Typography
+              sx={{
+                fontSize: "30px",
+                fontWeight: "500",
+                textAlign: "center",
+                color: "#202B52",
+                fontFamily: "Roboto Condensed",
+              }}
+            >
+              <strong>Editar area </strong>
+            </Typography>
+          </Box>
+        </Box>
+        <Divider
+          style={{
+            marginLeft: "5%",
+            marginRight: "5%",
+            borderColor: "#202B52",
+          }}
+        />
+        <CardContent sx={{ padding: "30px" }}>
+          <form onSubmit={actualizar}>
+            <Typography
+              variant="h6"
+              sx={{
+                fontFamily: "Roboto Condensed",
+                color: "#202B52",
+                fontSize: "16px",
+              }}
+            >
+              Nombre Area:
+            </Typography>
+            <TextField
+              value={var_nombreArea}
+              onChange={(e) => setVar_nombreArea(e.target.value)}
+              fullWidth
+              sx={{ mb: 2 }}
+              InputProps={{
+                sx: {
+                  height: "40px",
+                  fontFamily: "Roboto Condensed",
+                  fontSize: "16px",
+                },
+              }}
+            />
 
-}
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                gap: "10px",
+              }}
+            >
+              <Button
+                sx={{ backgroundColor: "#202B52", fontFamily: "poppins" }}
+                variant="contained"
+                type="submit"
+              >
+                Guardar
+              </Button>
+              <Button
+                sx={{ backgroundColor: "#ff0000", fontFamily: "poppins" }}
+                variant="contained"
+                onClick={handleGoBack}
+              >
+                Volver
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
 
-export default CompEditarEstructuraOrganizacional
+export default CompEditarEstructuraOrganizacional;
