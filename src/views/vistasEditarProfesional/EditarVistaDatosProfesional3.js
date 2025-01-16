6064594738776ad2db1ea3a1f9d9c29e905c6959
PRACTICANTE 1 SOPORTE <soporte.tecnico@ESUMER.LOCAL>
2025-01-16 18:03:47 -0500

    faltan 3 modulos

--- src/views/vistasEditarProfesional/EditarVistaDatosProfesional3.js
+++ src/views/vistasEditarProfesional/EditarVistaDatosProfesional3.js
@@ -19,6 +19,11 @@ const EditarDatosProfesional3 = () => {
     const [serviciosSaludAdicionalOptions, setServiciosSaludAdicionalOptions] = useState([]);
     const [selectedServiciosSaludAdicional, setSelectedServiciosSaludAdicional] = useState([]);
     const [prevSelectedServiciosSaludAdicional, setPrevSelectedServiciosSaludAdicional] = useState([]);
+    const [antecedentesOptions, setAntecedentesOptions] = useState([]);
+    const [selectedAntecedentes, setSelectedAntecedentes] = useState([]);
+    const [prevSelectedAntecedentesMedicos, setPrevSelectedAntecedentesMedicos] = useState([]);
+
+
     const navigate = useNavigate();
 
     // Obtener el ID desde localStorage
@@ -76,58 +81,32 @@ const EditarDatosProfesional3 = () => {
         fetchServiciosSaludAdicional();
     }, []);
 
+    // Hacer la solicitud para obtener los antecedentes médicos
+    useEffect(() => {
+        const fetchAntecedentes = async () => {
+            try {
+                const response = await axios.get('https://evaluacion.esumer.edu.co/api/antecedentesMedicos/');
+                setAntecedentesOptions(response.data);
+            } catch (error) {
+                console.error('Error al obtener los antecedentes médicos:', error);
+            }
+        };
 
-    const manejarCambioServicios = async (event) => {
-        const { value } = event.target; // Nuevas opciones seleccionadas
+        fetchAntecedentes();
+    }, []);
+
+
+    // Manejar cambios de selección en el estado local
+    const manejarCambioServicios = (event) => {
+        const { value } = event.target;
         setSelectedServiciosSaludAdicional(value);
-
-        // Eliminar servicios deseleccionados
-        const serviciosParaEliminar = prevSelectedServiciosSaludAdicional.filter(
-            (id_servicio) => !value.includes(id_servicio)
-        );
-
-        for (let id_servicio of serviciosParaEliminar) {
-            // Agregar log para revisar el valor de id_servicio
-            console.log("Eliminando servicio con id:", id_servicio);
-            try {
-                await axios.delete(`https://evaluacion.esumer.edu.co/api/profesionalServicioSaludAdicional/${id_profesionalPK}/${id_servicio}`);
-                console.log(`Relación eliminada: Profesional ID ${id_profesionalPK}, Servicio ID ${id_servicio}`);
-            } catch (error) {
-                console.error('Error al eliminar servicio:', error);
-            }
-        }
-
-        // Agregar nuevos servicios seleccionados
-        const serviciosParaAgregar = value.filter(
-            (id_servicio) => !prevSelectedServiciosSaludAdicional.includes(id_servicio)
-        );
-
-        for (let id_servicio of serviciosParaAgregar) {
-            // Agregar log para revisar el valor de id_servicio
-            console.log("Agregando servicio con id:", id_servicio);
-            console.log("Agregando servicio con id_profesional:", id_profesionalPK);
-            try {
-                await axios.post(`http://localhost:3001/profesionalServicioSaludAdicional/`, {
-                    id_profesionalFK: id_profesionalPK,
-                    id_ServicioDeSaludAdicionalFK: id_servicio
-                });
-                console.log(`Relación creada: Profesional ID ${id_profesionalPK}, Servicio ID ${id_servicio}`);
-            } catch (error) {
-                console.error('Error al agregar servicio:', error);
-            }
-        }
-
-        // Actualizar el estado previo
-        setPrevSelectedServiciosSaludAdicional(value);
     };
 
+    const manejarCambioAntecedentes = (event) => {
+        const { value } = event.target;
+        setSelectedAntecedentes(value);
+    };
 
-    useEffect(() => {
-        // Inicializar el estado previo con los servicios ya asociados al profesional
-        if (id_profesionalPK) {
-            fetchServiciosProfesional();
-        }
-    }, [id_profesionalPK]);
 
     // Cuando los servicios asociados al profesional se obtienen correctamente
     const fetchServiciosProfesional = async () => {
@@ -136,30 +115,62 @@ const EditarDatosProfesional3 = () => {
             const response = await axios.get(
                 `https://evaluacion.esumer.edu.co/api/profesionalServicioSaludAdicional/${id_profesionalPK}`
             );
-                
+
             // Extraer los IDs de los servicios asociados
             const serviciosIds = response.data.map(
                 (servicio) => servicio.id_ServicioDeSaludAdicionalFK
             );
-    
-            console.log("Servicios IDs extraídos:", serviciosIds); 
-    
+
+            console.log("Servicios IDs extraídos:", serviciosIds);
+
             // Sincronizar el estado de los servicios seleccionados
             setSelectedServiciosSaludAdicional(serviciosIds);
             setPrevSelectedServiciosSaludAdicional(serviciosIds);
-    
+
             // Sincronizar los nombres con los IDs
             const selectedNames = serviciosSaludAdicionalOptions
                 .filter((servicio) => serviciosIds.includes(servicio.id_servicioDeSaludAdicionalPK))
                 .map((servicio) => servicio.var_nombreServicioDeSaludAdicional);
-    
+
             console.log('Servicios seleccionados por nombre:', selectedNames); // Verifica los nombres
-    
+
         } catch (error) {
             console.error('Error al obtener los servicios de salud adicional del profesional:', error);
         }
     };
-    
+
+    // Cuando los antecedes asociados al profesional se obtienen correctamente
+    const fetchAntecedentesMedicos = async () => {
+        try {
+            // Obtener los servicios asociados al profesional
+            const response = await axios.get(
+                `https://evaluacion.esumer.edu.co/api/profesionalAntecedenteMedico/${id_profesionalPK}`
+            );
+
+            // Extraer los IDs de los servicios asociados
+            const antecedentesIds = response.data.map(
+                (antecedente) => antecedente.id_antecedenteMedicoFK
+            );
+
+            console.log("antecedentes IDs extraídos:", antecedentesIds);
+
+            // Sincronizar el estado de los servicios seleccionados
+            setSelectedAntecedentes(antecedentesIds);
+            setPrevSelectedAntecedentesMedicos(antecedentesIds);
+
+            // Sincronizar los nombres con los IDs
+            const selectedNames = antecedentesOptions
+                .filter((antecedente) => antecedentesIds.includes(antecedente.id_antecedenteMedicoPK))
+                .map((antecedente) => antecedente.var_nombreAntecedenteMedico);
+
+            console.log('antecedentes seleccionados por nombre:', selectedNames); // Verifica los nombres
+
+        } catch (error) {
+            console.error('Error al obtener los antecedentes medicos del profesional:', error);
+        }
+    };
+
+
 
 
 
@@ -173,7 +184,22 @@ const EditarDatosProfesional3 = () => {
         if (fondoPensionOptions.length > 0) {
             getUsuarios();
         }
-    }, [epsOptions, fondoPensionOptions]);
+
+        // Inicializar el estado previo con los servicios ya asociados al profesional
+        if (id_profesionalPK) {
+            fetchServiciosProfesional();
+        }
+
+        if (antecedentesOptions.length > 0 && id_profesionalPK) {
+            fetchAntecedentesMedicos();
+        }
+    }, [epsOptions, fondoPensionOptions, id_profesionalPK, antecedentesOptions]);
+
+    useEffect(() => {
+        // Asegúrate de que el valor inicial sea un array vacío
+        setSelectedAntecedentes([]);
+    }, []);
+
 
 
 
@@ -200,6 +226,76 @@ const EditarDatosProfesional3 = () => {
             }
         } catch (error) {
             console.error('Error al obtener los datos del profesional:', error);
+        }
+    };
+
+    // Guardar cambios en el servidor
+    const guardarCambios = async () => {
+        const serviciosParaEliminar = prevSelectedServiciosSaludAdicional.filter(
+            (id_servicio) => !selectedServiciosSaludAdicional.includes(id_servicio)
+        );
+        const serviciosParaAgregar = selectedServiciosSaludAdicional.filter(
+            (id_servicio) => !prevSelectedServiciosSaludAdicional.includes(id_servicio)
+        );
+
+        const antecedenteParaEliminar = prevSelectedAntecedentesMedicos.filter(
+            (id_antecedente) => !selectedAntecedentes.includes(id_antecedente)
+        );
+        const antecedenteParaAgregar = selectedAntecedentes.filter(
+            (id_antecedente) => !prevSelectedAntecedentesMedicos.includes(id_antecedente)
+        );
+
+        try {
+            // Eliminar servicios deseleccionados
+            for (let id_servicio of serviciosParaEliminar) {
+                await axios.delete(
+                    `https://evaluacion.esumer.edu.co/api/profesionalServicioSaludAdicional/${id_profesionalPK}/${id_servicio}`
+                );
+                console.log(`Servicio eliminado: ${id_servicio}`);
+            }
+
+            // Agregar nuevos servicios seleccionados
+            for (let id_servicio of serviciosParaAgregar) {
+                await axios.post("http://localhost:3001/profesionalServicioSaludAdicional/", {
+                    id_profesionalFK: id_profesionalPK,
+                    id_ServicioDeSaludAdicionalFK: id_servicio,
+                });
+                console.log(`Servicio agregado: ${id_servicio}`);
+            }
+
+            // Actualizar el estado previo
+            setPrevSelectedServiciosSaludAdicional(selectedServiciosSaludAdicional);
+            console.log("Cambios guardados con éxito");
+
+            //-----------------
+
+
+            // Eliminar antecedentes deseleccionados
+            for (let id_antecedente of antecedenteParaEliminar) {
+                await axios.delete(
+                    `https://evaluacion.esumer.edu.co/api/profesionalAntecedenteMedico/${id_profesionalPK}/${id_antecedente}`
+                );
+                console.log(`antecedente eliminado: ${id_antecedente}`);
+            }
+
+            // Agregar nuevos servicios seleccionados
+            for (let id_antecedente of antecedenteParaAgregar) {
+                await axios.post("http://localhost:3001/profesionalAntecedenteMedico/", {
+                    id_profesionalFK: id_profesionalPK,
+                    id_antecedenteMedicoFK: id_antecedente,
+                });
+                console.log(`antecedente agregado: ${id_antecedente}`);
+            }
+
+            // Actualizar el estado previo
+            setPrevSelectedAntecedentesMedicos(selectedAntecedentes);
+            console.log("Cambios guardados con éxito");
+
+
+
+
+        } catch (error) {
+            console.error("Error al guardar cambios:", error);
         }
     };
 
@@ -312,9 +408,7 @@ const EditarDatosProfesional3 = () => {
                                 name="selectedServiciosSaludAdicional"
                                 multiple
                                 value={selectedServiciosSaludAdicional}
-                                onChange={(event) =>
-                                    manejarCambioServicios(event, "serviciosSaludAdicional")
-                                }
+                                onChange={manejarCambioServicios}
                                 renderValue={(selected) => {
                                     const selectedNames = serviciosSaludAdicionalOptions
                                         .filter((servicio) =>
@@ -355,11 +449,67 @@ const EditarDatosProfesional3 = () => {
                                 ))}
                             </Select>
 
-                           
+
+                        </FormControl>
+
+                        <FormControl
+                            sx={{ mb: 2 }}
+                            fullWidth
+
+                        >
+                            <Typography
+                                variant="h6"
+                                sx={{ fontFamily: 'Roboto Condensed', color: '#202B52', fontSize: '16px' }}
+                            >
+                                Seleccione Antecedentes Médicos (se pueden seleccionar varias opciones):
+                            </Typography>
+
+                            <Select
+                                name="antecedentes"
+                                multiple
+                                value={selectedAntecedentes}
+                                onChange={manejarCambioAntecedentes}
+                                renderValue={(selected) => {
+                                    // Filtrar los antecedentes que están seleccionados y luego mapear solo los nombres
+                                    const selectedNames = antecedentesOptions
+                                        .filter((antecedente) =>
+                                            selected.includes(antecedente.id_antecedenteMedicoPK)
+                                        )
+                                        .map((antecedente) => antecedente.var_nombreAntecedenteMedico);
+
+                                    return selectedNames.join(" - "); // Unir los nombres con un guion
+                                }}
+                                fullWidth
+                                variant="outlined"
+                                MenuProps={{
+                                    PaperProps: { style: { maxHeight: 224, width: 250 } },
+                                }}
+                                sx={{
+                                    height: "40px",
+                                    fontFamily: "Roboto Condensed",
+                                    fontSize: "16px",
+                                }}
+                            >
+                                {antecedentesOptions.map((antecedente) => (
+                                    <MenuItem
+                                        key={antecedente.id_antecedenteMedicoPK}
+                                        value={antecedente.id_antecedenteMedicoPK}
+                                    >
+                                        
+                                        <Checkbox
+                                            checked={selectedAntecedentes.indexOf(antecedente.id_antecedenteMedicoPK) > -1}
+                                        />
+
+                                        <ListItemText
+                                            primary={antecedente.var_nombreAntecedenteMedico}
+                                        />
+                                    </MenuItem>
+                                ))}
+                            </Select>
                         </FormControl>
 
                         <div style={{ display: "flex", justifyContent: "flex-end" }}>
-                            <Button sx={{ backgroundColor: "#202B52", fontFamily: 'poppins' }} variant="contained" type="submit">
+                            <Button sx={{ backgroundColor: "#202B52", fontFamily: 'poppins' }} variant="contained" type="submit" onClick={guardarCambios}>
                                 Guardar
                             </Button>
                         </div>
@@ -373,30 +523,5 @@ const EditarDatosProfesional3 = () => {
 }
 
 export default EditarDatosProfesional3
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
--- src/views/vistasEditarProfesional/EditarVistaDatosProfesional4.js
+++ src/views/vistasEditarProfesional/EditarVistaDatosProfesional4.js
@@ -1,17 +1,21 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Card, CardContent, Divider, Box, Typography, TextField, Button } from "@mui/material";
+import { Card, CardContent, Divider, Select, MenuItem, FormControl, Box, Typography, TextField, Button } from "@mui/material";
 
 const URI_PROFESIONAL = 'http://localhost:3001/profesional/';
 const URI_PROFESIONAL_POR_ID_USUARIO = 'http://localhost:3001/profesional/porUsuario/';
 
 const EditarDatosProfesional4 = () => {
-    const [id_profesionalPK, setId_profesionalPK]= useState()
-    const [id_cuentaBancariaFK, setId_cuentaBancariaFK]= useState()
-    const [var_tipoCuenta, setVar_tipoCuenta]= useState()
+    const [id_profesionalPK, setId_profesionalPK] = useState()
+    const [id_cuentaBancariaFK, setId_cuentaBancariaFK] = useState()
+    const [var_tipoCuenta, setVar_tipoCuenta] = useState()
     const [var_numeroCuenta, setVar_numeroCuenta] = useState('');
-   
+    const [selectedBanco, setSelectedBanco] = useState("");
+    const [bancos, setBancos] = useState([]);
+
+
+
     const navigate = useNavigate();
 
     // Obtener el ID desde localStorage
@@ -21,7 +25,7 @@ const EditarDatosProfesional4 = () => {
     const actualizar = async (e) => {
         e.preventDefault();
         await axios.put(URI_PROFESIONAL + id_profesionalPK, {
-            id_cuentaBancariaFK: id_cuentaBancariaFK,
+            id_cuentaBancariaFK: selectedBanco,
             var_tipoCuenta: var_tipoCuenta,
             var_numeroCuenta: var_numeroCuenta,
 
@@ -39,10 +43,35 @@ const EditarDatosProfesional4 = () => {
         setId_cuentaBancariaFK(res.data.id_cuentaBancariaFK);
         setVar_tipoCuenta(res.data.var_tipoCuenta);
         setVar_numeroCuenta(res.data.var_numeroCuenta);
-       
+
 
     };
+
+    const manejarCambio = (event) => {
+        const { value } = event.target;
+        setSelectedBanco(value);  // Esto actualiza el estado de selectedBanco
+    };
     
+
+    // useEffect para obtener los bancos desde el servidor
+    useEffect(() => {
+        const fetchBancos = async () => {
+            try {
+                const response = await axios.get("https://evaluacion.esumer.edu.co/api/cuentasBancarias");
+                setBancos(response.data);
+                // Establecer el banco seleccionado si ya tienes el id_cuentaBancariaFK
+                if (id_cuentaBancariaFK) {
+                    setSelectedBanco(id_cuentaBancariaFK); // Esto hará que se seleccione el banco correcto en el Select
+                    
+                }
+            } catch (error) {
+                console.error("Error al obtener los bancos:", error);
+            }
+        };
+
+        fetchBancos();
+    }, [id_cuentaBancariaFK]);
+
     return (
         <div style={{ backgroundColor: "#F2F2F2", paddingTop: "3%", paddingBottom: "3%" }}>
             <div style={{ textAlign: "center", marginBottom: "1%", marginTop: "-1%" }}>
@@ -60,15 +89,40 @@ const EditarDatosProfesional4 = () => {
                 <CardContent sx={{ padding: "30px" }}>
                     <form onSubmit={actualizar}>
 
-                    <Typography variant="h6" sx={{ fontFamily: 'Roboto Condensed', color: '#202B52', fontSize: '16px' }}>banco:</Typography>
-                        <TextField
-                            value={id_cuentaBancariaFK}
-                            onChange={(e) => setId_cuentaBancariaFK(e.target.value)}
+                        <FormControl
                             fullWidth
                             sx={{ mb: 2 }}
-                            InputProps={{ sx: { height: "40px", fontFamily: "Roboto Condensed", fontSize: "16px" } }}
-                        />
-                    
+                        >
+                            <Typography
+                                variant="h6"
+                                sx={{ fontFamily: 'Roboto Condensed', color: '#202B52', fontSize: '16px' }}
+                            >
+                                Seleccione Banco:
+                            </Typography>
+
+                            <Select
+                                name="banco"
+                                value={selectedBanco}
+                                onChange={manejarCambio}
+                                sx={{
+                                    height: "40px",
+                                    fontFamily: "Roboto Condensed",
+                                    fontSize: "16px",
+                                }}
+                            >
+                                {bancos.map((banco) => (
+                                    <MenuItem
+                                        key={banco.id_cuentaBancariaPK}
+                                        value={banco.id_cuentaBancariaPK}
+                                    >
+                                        {" "}
+                                        {banco.var_nombreCuentaBancaria}{" "}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+
+                        </FormControl>
+
                         <Typography variant="h6" sx={{ fontFamily: 'Roboto Condensed', color: '#202B52', fontSize: '16px' }}>tipo cuenta:</Typography>
                         <TextField
                             value={var_tipoCuenta}
@@ -87,7 +141,7 @@ const EditarDatosProfesional4 = () => {
                             InputProps={{ sx: { height: "40px", fontFamily: "Roboto Condensed", fontSize: "16px" } }}
                         />
 
-                        
+
 
 
                         <div style={{ display: "flex", justifyContent: "flex-end" }}>
