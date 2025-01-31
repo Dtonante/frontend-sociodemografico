ba35ca8c5aa2d9415e2af50c5510cb0795d22ab6
PRACTICANTE 2 SOPORTE <practicantesoporte@ESUMER.LOCAL>
2025-01-31 16:38:50 -0500

    alerts validaciones hasta servicios que no cuentan en editar admin

--- src/views/serviciosQueNoCuentan/EditServiciosQueNoCuentan.js
+++ src/views/serviciosQueNoCuentan/EditServiciosQueNoCuentan.js
@@ -10,6 +10,7 @@ import {
   TextField,
   Button,
 } from "@mui/material";
+import {showAlert,show_alert} from '../../components/showAlert/alertFuntion'
 
 const URI_SERVICIO_QUE_NO_CUENTAN =
   "http://localhost:3001/serviciosQueNoCuentan/";
@@ -22,14 +23,106 @@ const CompEditarServiciosQueNoCuentan = () => {
     navigate("/app/serviciosQueNoCuentan");
   };
   const { id_servicioQueNoCuentaPK } = useParams();
+  const [errorServicioQueNoCuenta, setErrorServicioQueNoCuenta] = useState(false);
+  const [error, setError] = useState(false);
+
+
 
   //procedimiento para actualizar
   const actualizar = async (e) => {
     e.preventDefault();
-    await axios.put(URI_SERVICIO_QUE_NO_CUENTAN + id_servicioQueNoCuentaPK, {
-      var_nombreServicioQueNoCuenta: var_nombreServicioQueNoCuenta,
+
+    const camposObligatorios = [
+      { nombre: "var_nombreServicioQueNoCuenta", valor: var_nombreServicioQueNoCuenta}
+    ];
+
+    // Recorremos los campos para validar que no estén vacíos
+    let camposValidos = true;
+    camposObligatorios.forEach((campo) => {
+      // Verificamos si el valor no es nulo o indefinido y si es una cadena
+      if (typeof campo.valor === "string" && campo.valor.trim() === "") {
+        setError((prevState) => ({
+          ...prevState,
+          [campo.nombre]: true, // Marcamos el error para el campo específico
+        }));
+        camposValidos = false;
+      } else if (
+        campo.valor === null ||
+        campo.valor === undefined ||
+        campo.valor === ""
+      ) {
+        setError((prevState) => ({
+          ...prevState,
+          [campo.nombre]: true, // Marcamos el error para el campo específico
+        }));
+        camposValidos = false;
+      } else {
+        setError((prevState) => ({
+          ...prevState,
+          [campo.nombre]: false, // Si el campo tiene valor, eliminamos el error
+        }));
+      }
     });
-    navigate("/app/serviciosQueNoCuentan");
+
+    if (!camposValidos) {
+      // Si algún campo es inválido, mostramos la alerta y detenemos el proceso
+      show_alert("Por favor, completa todos los campos obligatorios.", "info");
+      return; // Detenemos el proceso si algún campo requerido está vacío
+    }
+
+    showAlert(
+      {
+        title: "¿Estás seguro?",
+        text: "¿Deseas guardar los cambios realizados?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Sí, guardar",
+        cancelButtonText: "Cancelar",
+      },
+      async () => {
+        try {
+          await axios.put(URI_SERVICIO_QUE_NO_CUENTAN + id_servicioQueNoCuentaPK, {
+            var_nombreServicioQueNoCuenta: var_nombreServicioQueNoCuenta,
+          });
+          show_alert("Cambios guardados exitosamente", "success");
+          navigate("/app/serviciosQueNoCuentan");
+        } catch (error) {
+          show_alert("Error al guardar los cambios", "error");
+        }
+      },
+      () => {
+        show_alert("Cambios cancelados", "info");
+      }
+    );
+    
+    
+  };
+
+  const validarCampoRequerido = (valor, setError) => {
+    if (!valor || valor.trim() === "") {
+      setError(true); // Establece el error si el campo está vacío
+      return false; // Retorna false para indicar que la validación falló
+    } else {
+      setError(false); // Si el campo tiene valor, quita el error
+      return true; // Retorna true si la validación pasa
+    }
+  };
+
+  const handleKeyPress = (event, fieldName) => {
+    let regex;
+
+    if (
+      fieldName === "var_nombreServicioQueNoCuenta" 
+    ) {
+      // Solo permitimos letras (incluyendo acentos y ñ) y espacios
+      regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]*$/;
+    }
+
+    // Verificamos si se ha definido una expresión regular para el campo
+    if (regex && !regex.test(event.key)) {
+      event.preventDefault(); // Evita la entrada de caracteres no válidos
+      return; // Salimos de la función si el carácter no es válido
+    }
   };
 
   useEffect(() => {
@@ -42,6 +135,8 @@ const CompEditarServiciosQueNoCuentan = () => {
     );
     setVar_nombreServicioQueNoCuenta(res.data.var_nombreServicioQueNoCuenta);
   };
+
+  
 
   return (
     <div
@@ -107,7 +202,17 @@ const CompEditarServiciosQueNoCuentan = () => {
             </Typography>
             <TextField
               value={var_nombreServicioQueNoCuenta}
-              onChange={(e) => setVar_nombreServicioQueNoCuenta(e.target.value)}
+              
+              onKeyPress={(event) => handleKeyPress(event, "var_nombreServicioQueNoCuenta")}
+              onChange={(e) => {
+                const valor = e.target.value;
+                setVar_nombreServicioQueNoCuenta(valor);
+
+                // Validar si el campo está vacío al cambiar el valor
+                validarCampoRequerido(valor, setErrorServicioQueNoCuenta);
+              }}
+              error={errorServicioQueNoCuenta} // Mostrar borde rojo si hay error
+              helperText={errorServicioQueNoCuenta ? "Este campo es obligatorio" : ""} // Mensaje de error
               fullWidth
               sx={{ mb: 2 }}
               InputProps={{
@@ -119,7 +224,13 @@ const CompEditarServiciosQueNoCuentan = () => {
               }}
             />
 
-            <div style={{ display: "flex", justifyContent: "flex-end", gap: "10px" }}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                gap: "10px",
+              }}
+            >
               <Button
                 sx={{ backgroundColor: "#202B52", fontFamily: "poppins" }}
                 variant="contained"
