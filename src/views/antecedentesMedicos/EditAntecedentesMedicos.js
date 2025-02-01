80adbf9251e03b0b52e5c14fee90eefe7849fd2d
LeoHrz32 <herreraleo320gmail.com>
2025-02-01 09:52:23 -0500

    todas las alertas listar

--- src/views/antecedentesMedicos/EditAntecedentesMedicos.js
+++ src/views/antecedentesMedicos/EditAntecedentesMedicos.js
@@ -10,6 +10,9 @@ import {
   TextField,
   Button,
 } from "@mui/material";
+import {showAlert,show_alert} from '../../components/showAlert/alertFuntion';
+
+
 
 const URI_ANTECEDENTE_MEDICO = "http://localhost:3001/antecedentesMedicos/";
 
@@ -18,14 +21,79 @@ const CompEditarAntecedentesMedicos = () => {
     useState("");
   const navigate = useNavigate();
   const { id_antecedenteMedicoPK } = useParams();
+  const [errorAntecedenteMedico, setErrorAntecedenteMedico] = useState(false);
+  const [error, setError] = useState(false);
 
   //procedimiento para actualizar
   const actualizar = async (e) => {
     e.preventDefault();
-    await axios.put(URI_ANTECEDENTE_MEDICO + id_antecedenteMedicoPK, {
-      var_nombreAntecedenteMedico: var_nombreAntecedenteMedico,
+    const camposObligatorios = [
+      {
+        nombre: "var_nombreAntecedenteMedico",
+        valor: var_nombreAntecedenteMedico,
+      },
+    ];
+
+    // Recorremos los campos para validar que no estén vacíos
+    let camposValidos = true;
+    camposObligatorios.forEach((campo) => {
+      // Verificamos si el valor no es nulo o indefinido y si es una cadena
+      if (typeof campo.valor === "string" && campo.valor.trim() === "") {
+        setError((prevState) => ({
+          ...prevState,
+          [campo.nombre]: true, // Marcamos el error para el campo específico
+        }));
+        camposValidos = false;
+      } else if (
+        campo.valor === null ||
+        campo.valor === undefined ||
+        campo.valor === ""
+      ) {
+        setError((prevState) => ({
+          ...prevState,
+          [campo.nombre]: true, // Marcamos el error para el campo específico
+        }));
+        camposValidos = false;
+      } else {
+        setError((prevState) => ({
+          ...prevState,
+          [campo.nombre]: false, // Si el campo tiene valor, eliminamos el error
+        }));
+      }
     });
-    navigate("/app/antecedentesMedicos");
+
+    if (!camposValidos) {
+      // Si algún campo es inválido, mostramos la alerta y detenemos el proceso
+      show_alert("Por favor, completa todos los campos obligatorios.", "info");
+      return; // Detenemos el proceso si algún campo requerido está vacío
+    }
+
+    showAlert(
+      {
+        title: "¿Estás seguro?",
+        text: "¿Deseas guardar los cambios realizados?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Sí, guardar",
+        cancelButtonText: "Cancelar",
+      },
+      async () => {
+        try {
+          await axios.put(URI_ANTECEDENTE_MEDICO + id_antecedenteMedicoPK, {
+            var_nombreAntecedenteMedico: var_nombreAntecedenteMedico,
+          });
+          show_alert("Cambios guardados exitosamente", "success");
+          navigate("/app/antecedentesMedicos");
+        } catch (error) {
+          show_alert("Error al guardar los cambios", "error");
+        }
+      },
+      () => {
+        show_alert("Cambios cancelados", "info");
+      }
+    );
+    
+    
   };
 
   useEffect(() => {
@@ -39,9 +107,35 @@ const CompEditarAntecedentesMedicos = () => {
     setVar_nombreAntecedenteMedico(res.data.var_nombreAntecedenteMedico);
   };
 
-
   const handleGoBack = () => {
     navigate("/app/antecedentesMedicos");
+  };
+
+  const validarCampoRequerido = (valor, setError) => {
+    if (!valor || valor.trim() === "") {
+      setError(true); // Establece el error si el campo está vacío
+      return false; // Retorna false para indicar que la validación falló
+    } else {
+      setError(false); // Si el campo tiene valor, quita el error
+      return true; // Retorna true si la validación pasa
+    }
+  };
+
+  const handleKeyPress = (event, fieldName) => {
+    let regex;
+
+    if (
+      fieldName === "var_nombreCuentaBancaria" 
+    ) {
+      // Solo permitimos letras (incluyendo acentos y ñ) y espacios
+      regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]*$/;
+    }
+
+    // Verificamos si se ha definido una expresión regular para el campo
+    if (regex && !regex.test(event.key)) {
+      event.preventDefault(); // Evita la entrada de caracteres no válidos
+      return; // Salimos de la función si el carácter no es válido
+    }
   };
 
   return (
@@ -99,7 +193,16 @@ const CompEditarAntecedentesMedicos = () => {
             </Typography>
             <TextField
               value={var_nombreAntecedenteMedico}
-              onChange={(e) => setVar_nombreAntecedenteMedico(e.target.value)}
+              onKeyPress={(event) => handleKeyPress(event, "var_nombreAntecedenteMedico")}
+              onChange={(e) => {
+                const valor = e.target.value;
+                setVar_nombreAntecedenteMedico(valor);
+
+                // Validar si el campo está vacío al cambiar el valor
+                validarCampoRequerido(valor, setErrorAntecedenteMedico);
+              }}
+              error={errorAntecedenteMedico} // Mostrar borde rojo si hay error
+              helperText={errorAntecedenteMedico ? "Este campo es obligatorio" : ""} // Mensaje de error
               fullWidth
               sx={{ mb: 2 }}
               InputProps={{
--- src/views/cuentasBancarias/EditCuentasBancarias.js
+++ src/views/cuentasBancarias/EditCuentasBancarias.js
@@ -10,6 +10,7 @@ import {
   TextField,
   Button,
 } from "@mui/material";
+import {showAlert,show_alert} from '../../components/showAlert/alertFuntion';
 
 
 const URI_CUENTAS_BANCARIAS = 'http://localhost:3001/cuentasBancarias/'
@@ -18,14 +19,80 @@ const CompEditarCuentaBancaria = () => {
   const [var_nombreCuentaBancaria, setVar_nombreCuentaBancaria] = useState('')
   const navigate = useNavigate()
   const { id_cuentaBancariaPK } = useParams()
+  const [errorCuentasBancarias, setErrorCuentasBancarias] = useState(false);
+  const [error, setError] = useState(false);
 
   //procedimiento para actualizar
   const actualizar = async (e) => {
     e.preventDefault()
-    await axios.put(URI_CUENTAS_BANCARIAS + id_cuentaBancariaPK, {
-      var_nombreCuentaBancaria: var_nombreCuentaBancaria
-    })
-    navigate('/app/cuentasBancarias')
+
+    const camposObligatorios = [
+      {
+        nombre: "var_nombreCuentaBancaria",
+        valor: var_nombreCuentaBancaria,
+      },
+    ];
+
+    // Recorremos los campos para validar que no estén vacíos
+    let camposValidos = true;
+    camposObligatorios.forEach((campo) => {
+      // Verificamos si el valor no es nulo o indefinido y si es una cadena
+      if (typeof campo.valor === "string" && campo.valor.trim() === "") {
+        setError((prevState) => ({
+          ...prevState,
+          [campo.nombre]: true, // Marcamos el error para el campo específico
+        }));
+        camposValidos = false;
+      } else if (
+        campo.valor === null ||
+        campo.valor === undefined ||
+        campo.valor === ""
+      ) {
+        setError((prevState) => ({
+          ...prevState,
+          [campo.nombre]: true, // Marcamos el error para el campo específico
+        }));
+        camposValidos = false;
+      } else {
+        setError((prevState) => ({
+          ...prevState,
+          [campo.nombre]: false, // Si el campo tiene valor, eliminamos el error
+        }));
+      }
+    });
+
+    if (!camposValidos) {
+      // Si algún campo es inválido, mostramos la alerta y detenemos el proceso
+      show_alert("Por favor, completa todos los campos obligatorios.", "info");
+      return; // Detenemos el proceso si algún campo requerido está vacío
+    }
+
+    showAlert(
+      {
+        title: "¿Estás seguro?",
+        text: "¿Deseas guardar los cambios realizados?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Sí, guardar",
+        cancelButtonText: "Cancelar",
+      },
+      async () => {
+        try {
+          await axios.put(URI_CUENTAS_BANCARIAS + id_cuentaBancariaPK, {
+            var_nombreCuentaBancaria: var_nombreCuentaBancaria
+          })
+          show_alert("Cambios guardados exitosamente", "success");
+          navigate('/app/cuentasBancarias')
+        } catch (error) {
+          show_alert("Error al guardar los cambios", "error");
+        }
+      },
+      () => {
+        show_alert("Cambios cancelados", "info");
+      }
+    );
+    
+    
   }
 
   useEffect(() => {
@@ -39,6 +106,33 @@ const CompEditarCuentaBancaria = () => {
 
   const handleGoBack = () => {
     navigate("/app/cuentasBancarias");
+  };
+
+  const validarCampoRequerido = (valor, setError) => {
+    if (!valor || valor.trim() === "") {
+      setError(true); // Establece el error si el campo está vacío
+      return false; // Retorna false para indicar que la validación falló
+    } else {
+      setError(false); // Si el campo tiene valor, quita el error
+      return true; // Retorna true si la validación pasa
+    }
+  };
+
+  const handleKeyPress = (event, fieldName) => {
+    let regex;
+
+    if (
+      fieldName === "var_nombreCuentaBancaria" 
+    ) {
+      // Solo permitimos letras (incluyendo acentos y ñ) y espacios
+      regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]*$/;
+    }
+
+    // Verificamos si se ha definido una expresión regular para el campo
+    if (regex && !regex.test(event.key)) {
+      event.preventDefault(); // Evita la entrada de caracteres no válidos
+      return; // Salimos de la función si el carácter no es válido
+    }
   };
 
   return (
@@ -97,7 +191,17 @@ const CompEditarCuentaBancaria = () => {
             </Typography>
             <TextField
               value={var_nombreCuentaBancaria}
-              onChange={(e) => setVar_nombreCuentaBancaria(e.target.value)}
+
+              onKeyPress={(event) => handleKeyPress(event, "var_nombreDocumento")}
+              onChange={(e) => {
+                const valor = e.target.value;
+                setVar_nombreCuentaBancaria(valor);
+
+                // Validar si el campo está vacío al cambiar el valor
+                validarCampoRequerido(valor, setErrorCuentasBancarias);
+              }}
+              error={errorCuentasBancarias} // Mostrar borde rojo si hay error
+              helperText={errorCuentasBancarias ? "Este campo es obligatorio" : ""} // Mensaje de error
               fullWidth
               sx={{ mb: 2 }}
               InputProps={{
